108c1ddd1495713db3d80b5846f7b1d69f848940
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-12-11 15:35:13 -0800

    adding tests

--- src/EcoSystem.java
+++ src/EcoSystem.java
@@ -13,15 +13,20 @@ public class EcoSystem {
     private static List<Coordinates2D> firstGenOccupiedLocations = new ArrayList<>();
     private static List<Coordinates2D> nextGenOccupiedLocations = new ArrayList<>();
 
+    /**
+     * Generate the organisms based on the rules continuously.
+     * @param args
+     */
     public static void main(String[] args) {
+        // keep getting the simulation continuously.
         while (true) {
-            generateFirstGenOrganisms(50);
-            introduceDelay(1500);
+            generateFirstGenOrganisms(150);
+            introduceDelay(500);
             System.out.println("First Gen:" + firstGenOccupiedLocations);
             clearScreen();
 
-            generateNextGenOrganisms(30);
-            introduceDelay(500);
+            generateNextGenOrganisms(100);
+            introduceDelay(100);
             System.out.println("Second gen: " + nextGenOccupiedLocations);
 
             clearFirstGenLocations();
@@ -49,31 +54,57 @@ public class EcoSystem {
         }
     }
 
+    /**
+     * Generate the second generations and beyond organisms
+     * @param numberOfOrganisms number of organisms to be generated.
+     */
     private static void generateNextGenOrganisms(int numberOfOrganisms) {
          generateOrganisms(numberOfOrganisms,2);
     }
 
+    /**
+     * Generates the organisms based on the generation and accounts for
+     * neighborhood understanding and accommodation.
+     *
+     * @param numberOfOrganisms number of organisms that has to be used.
+     * @param generation the generation that has to be considered for.
+     */
     private static void generateOrganisms(int numberOfOrganisms, int generation) {
         for (int i = 0; i < numberOfOrganisms; i++) {
             int randX = randomNumberGenerator(0, WIDTH);
             int randY = randomNumberGenerator(0, HEIGHT);
 
-            //
+            // if first generation then directly paint
             if (generation ==1){
                 graphics.fillRect(randX, randY, ORGANISM_SIZE_PX, ORGANISM_SIZE_PX);
                 firstGenOccupiedLocations.add(new Coordinates2D(randX,randY));
-            }else{
+            }
 
-                graphics.fillRect(randX, randY, ORGANISM_SIZE_PX, ORGANISM_SIZE_PX);
+            // check if fillable in the neighbour hood and then do the process of filling.
+            else{
+                if(checkIfFillable(randX,randY,ORGANISM_SIZE_PX)) {
+                    graphics.fillRect(randX, randY, ORGANISM_SIZE_PX, ORGANISM_SIZE_PX);
+                }
+                // keep a track of new locations
                 nextGenOccupiedLocations.add(new Coordinates2D(randX,randY));
             }
         }
     }
 
+    /**
+     * Generates a random int in the limit [min, max].
+     * @param min least value that needs to be included during generation.
+     * @param max max value that needs to be included during generation.
+     * @return a random int
+     */
     public static int randomNumberGenerator(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
+    /**
+     * Generates the first gen organisms
+     * @param numberOfOrganisms count of organisms that needs to be generated.
+     */
     public static void generateFirstGenOrganisms(int numberOfOrganisms) {
         generateOrganisms(numberOfOrganisms,1);
     }
@@ -84,9 +115,9 @@ public class EcoSystem {
      * There was an organism at the location in the last generation and two of the eight
        neighboring locations also contained organisms;
      * Three of the eight neighboring locations contained organisms in the last generation.
-     * @param x
-     * @param y
-     * @return
+     * @param x x co-ordinate to check
+     * @param y y co-ordinate to check
+     * @return true if a new organism can be accommodated based on the rules.
      */
     public static boolean checkIfFillable(int x, int y, int organismSize){
         int occupancyCounter = 0;
@@ -132,10 +163,18 @@ public class EcoSystem {
         return (occupancyCounter == 3)||(occupancyCounter ==2);
     }
 
+    /**
+     * Gets the locations
+     * @return the filled first gen occupied spots.
+     */
     public static List<Coordinates2D> getFirstGenOccupiedLocations() {
         return firstGenOccupiedLocations;
     }
 
+    /**
+     * Sets the Occupied locations. (Used for testing).
+     * @param firstGenOccupiedLocations sets the value from the input.
+     */
     public static void setFirstGenOccupiedLocations(List<Coordinates2D> firstGenOccupiedLocations) {
         EcoSystem.firstGenOccupiedLocations = firstGenOccupiedLocations;
     }
