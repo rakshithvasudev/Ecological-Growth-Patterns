a4b55468e10de09e023d14814195fa070fd1d88a
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-12-11 17:47:58 -0800

    deserialized

--- tst/EcoSystemTest.java
+++ tst/EcoSystemTest.java
@@ -83,7 +83,7 @@ public class EcoSystemTest {
 
         List<Coordinates2D> firstLocations = new ArrayList<>();
 
-        // all points are in the vicinity of a given organism.
+        // add new points to the first locations
         firstLocations.add(new Coordinates2D(200,110));
         firstLocations.add(new Coordinates2D(80,90));
         firstLocations.add(new Coordinates2D(80,100));
@@ -105,8 +105,28 @@ public class EcoSystemTest {
     @Test
     public void DeserializationTest(){
         List<Coordinates2D> firstLocations = new ArrayList<>();
+        List<Coordinates2D> deserializedFirstLocations;
+
+        firstLocations.add(new Coordinates2D(123,110));
+        firstLocations.add(new Coordinates2D(345,90));
+        firstLocations.add(new Coordinates2D(280,80));
+        firstLocations.add(new Coordinates2D(180,60));
+        firstLocations.add(new Coordinates2D(580,50));
+        firstLocations.add(new Coordinates2D(380,90));
+        firstLocations.add(new Coordinates2D(450,10));
+
+        EcoSystem ecs = new EcoSystem();
+        ecs.setFirstGenOccupiedLocations(firstLocations);
 
 
+        ecs.serializeObjects();
+
+
+        deserializedFirstLocations = EcoSystem.DeserializeObjects();
+
+        System.out.println(deserializedFirstLocations);
+
+        Assert.assertTrue(deserializedFirstLocations.size()>0);
     }
 
 
